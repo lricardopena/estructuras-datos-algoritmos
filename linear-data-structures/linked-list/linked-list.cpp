7db5dc5bf217ff4b54ecdeab2cfe2a29e5c81d0e
lpenalla <lpenalla@everis.com>
2021-08-30 20:07:26 -0500

    Refactor the code

--- linear-data-structures/linked-list/linked-list.cpp
+++ linear-data-structures/linked-list/linked-list.cpp
@@ -3,16 +3,35 @@
 using namespace std;
 
 template <class T> class Node {
-    public:
+    private:
         T data; // The object information
         Node* next; // Pointer to the next node element
+        
+    public:
+        Node(T new_data, Node* next_node){
+            this->data = new_data;
+            this->next = next_node;
+        }
+
+        Node(T new_data){
+            this->data = new_data;
+            this->next = NULL;
+        }
 
         void set_data(T new_data){
             this->data = new_data;
         }
 
+        T get_data(){
+            return this->data;
+        }
+
         void set_next(Node *next_node){
             this->next = next_node;
+        }
+
+        Node* get_next(){
+            return this->next;
         }
 };
 
@@ -29,33 +48,27 @@ template <class T> class CustomLinkedList{
 
         }
 
-        // Method adds info to the end of the list
-        void add_end(T info){
+        // Method adds data to the end of the list
+        void add_end(T data){
             if(head == NULL){ //if our list is currently empty
-                head = new Node<T>; //Create new node of type T
-                head->set_data(info);
-                head->set_next(NULL);
+                head = new Node<T>(data); //Create new node of type T
             }
             else{ //if not empty add to the end and move the tail
-                Node<T>* temp = new Node<T>;
-                temp->set_data(info);
-                temp->set_next(NULL);
-
+                Node<T>* temp = new Node<T>(data);
+                
                 Node<T>* current_node = this->head;
-                while (current_node->next != NULL){
-                    current_node = current_node->next;
+                while (current_node->get_next() != NULL){
+                    current_node = current_node->get_next();
                 }
                 
-                current_node->next = temp;
+                current_node->set_next(temp);
             }
         }
 
-        // Method adds info to the begining of the list
-        void add_begin(T info){
-            Node<T>* temp = new Node<T>;
-            temp->set_data(info);
-            temp->set_next(head);
-
+        // Method adds data to the begining of the list
+        void add_begin(T data){
+            Node<T>* temp = new Node<T>(data, head);
+            
             this->head = temp;
         }
 
@@ -63,21 +76,18 @@ template <class T> class CustomLinkedList{
         void add_after_node(T value_insert, T value_target){
             Node<T>* current_node = this->head;
             // Search for the node to delete
-            while ( current_node && current_node->data != value_target){
-                current_node = current_node->next;
+            while(current_node && current_node->get_data() != value_target){
+                current_node = current_node->get_next();
             }
 
             // current node is empty the node wasn't found 
-            if(!current_node || current_node == NULL){
+            if(!current_node){
                 return;
             }
 
-            Node<T>* node_insert = new Node<T>;
-            node_insert->set_data(value_insert);
+            Node<T>* node_insert = new Node<T>(value_insert, current_node->get_next());
             
-            node_insert->set_next(current_node->next);
-
-            current_node->next = node_insert;
+            current_node->set_next(node_insert);
         }
 
 
@@ -89,14 +99,14 @@ template <class T> class CustomLinkedList{
             
             // Check if the node to delete is the head
             if(this->head->data == value){
-                this->head = this->head->next;
+                this->head = this->head->get_next();
                 return;
             }
 
             Node<T>* current_node = this->head;
             // Search for the node to delete
-            while ( current_node && current_node->next->data != value){
-                current_node = current_node->next;
+            while ( current_node && current_node->next->get_data() != value){
+                current_node = current_node->get_next();
             }
 
             // current node is empty the node wasn't found 
@@ -104,18 +114,18 @@ template <class T> class CustomLinkedList{
                 return;
             }
 
-            Node<T>* node_to_delete = current_node->next;
+            Node<T>* node_to_delete = current_node->get_next(), next_node = node_to_delete->get_next();
 
-            current_node->next = node_to_delete->next;
+            current_node->set_next(next_node);
             delete node_to_delete;
         }
 
         void print_list(){
             Node<T>* current_node = this->head;
             while (current_node != NULL){
-                cout << current_node ->data << " -> ";
+                cout << current_node->get_data() << " -> ";
 
-                current_node = current_node->next;
+                current_node = current_node->get_next();
             }
             cout << endl;
         }
